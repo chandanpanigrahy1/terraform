b870bfee161b8ace03033ead0f9be555710863f6
chandan <chandanpanigrahy24@gmail.com>
2022-06-06 21:12:24 +0530

    adding file

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 resource "aws_instance" "ec2" {
   ami           = "079b5e5b3971bd10d"
   instance_type = "t2.micro"
-  subnet_id     = subnet-0119d1f6bda0698fa
+  subnet_id     = "subnet-0119d1f6bda0698fa"
   tags = {
     Name = "HelloWorld"
     }
