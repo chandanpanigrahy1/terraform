6e8191be88bf78162185f5302a580fffb61be51c
chandanpanigrahy1 <104877360+chandanpanigrahy1@users.noreply.github.com>
2022-06-06 20:28:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "ec2" {
-  ami           = "ami-0c2b8ca1dad447f8a"
+  ami           = "079b5e5b3971bd10d"
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.mySubnet.id
+  subnet_id     = subnet-0119d1f6bda0698fa
   tags = {
     Name = "HelloWorld"
-  }
+    }
 }
