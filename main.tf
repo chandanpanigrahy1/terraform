53ed32e2119bde3f14b536af87cf44e6ac74506e
chandan <chandanpanigrahy24@gmail.com>
2022-06-07 08:21:10 +0530

    adding file

--- main.tf
+++ main.tf
@@ -1,11 +1,25 @@
+resource "aws_config_aggregate_authorization" "ak" {
+  account_id = "218009773641"
+  region     = "ap-south-1"
+}
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+# Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region     = "ap-south-1"
+  access_key = "AKIATFQTFEZE6VKZUC7R"
+  secret_key = "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"
 }
 resource "aws_instance" "ec2" {
-  ami           = "079b5e5b3971bd10d"
+  ami           = "ami-079b5e5b3971bd10d"
   instance_type = "t2.micro"
   subnet_id     = "subnet-0119d1f6bda0698fa"
   tags = {
     Name = "HelloWorld"
-    }
+  }
 }
