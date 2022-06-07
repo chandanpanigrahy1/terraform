5e1573f2420053e04132535810d8f2de427bef39
chandan <chandanpanigrahy24@gmail.com>
2022-06-07 20:00:32 +0530

    file updated.

--- main.tf
+++ main.tf
@@ -18,6 +18,7 @@ provider "aws" {
 resource "aws_instance" "ec2-instance" {
   ami           = "ami-079b5e5b3971bd10d"
   instance_type = "t2.micro"
+  subnet_id     = "subnet id -09057160738ffdd97"
   tags = {
     Name = "HelloWorld"
     }
